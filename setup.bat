58ab1ebf95f69557487f9258dfb6ed804fbed894
Tamino <roidingertamino@gmail.com>
2024-11-23 21:01:49 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal
 
+:: Pfad des aktuellen Skripts abrufen
+set "CURRENT_DIR=%~dp0"
+
 :: Check if Python is installed
 echo Checking for Python installation...
 for %%i in (python.exe) do set "PYTHON_PATH=%%~$PATH:i"
@@ -17,26 +20,21 @@ if "%PYTHON_PATH%"=="" (
 
 :: Upgrade pip and install required Python packages
 echo Installing Python packages...
-"%PYTHON_PATH%" -m pip install --upgrade pip
-"%PYTHON_PATH%" -m pip install yt-dlp
-"%PYTHON_PATH%" -m pip install pygame
-"%PYTHON_PATH%" -m pip install mutagen
-"%PYTHON_PATH%" -m pip install pypresence
+python -m pip install --upgrade pip
+python -m pip install yt-dlp pygame mutagen pypresence
 
 echo Python packages installed successfully.
 
-:: Check if ffmpeg is installed
-echo Checking for FFmpeg installation...
-ffmpeg -version >nul 2>&1
-if errorlevel 1 (
-    echo FFmpeg is not installed. Downloading and installing FFmpeg...
-    curl -L -o ffmpeg.zip https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
-    tar -xf ffmpeg.zip -C %cd%
-    setx PATH "%cd%\ffmpeg\bin;%PATH%"
-    del ffmpeg.zip
-    echo FFmpeg installed successfully.
+:: Check if ffmpeg is in the same directory as the script
+echo Checking for FFmpeg installation in the script directory...
+if exist "%CURRENT_DIR%ffmpeg.exe" (
+    echo FFmpeg is already in the script directory.
 ) else (
-    echo FFmpeg is already installed.
+    echo FFmpeg is not found in the script directory. Downloading and installing FFmpeg...
+    curl -L -o ffmpeg.zip https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
+    tar -xf ffmpeg.zip
+    del ffmpeg.zip
+    echo FFmpeg installed successfully in the script directory.
 )
 
 endlocal
