9733c8c685ba7d3c1a2adc2e68c1459f020e9977
Tamino <roidingertamino@gmail.com>
2024-11-26 12:07:02 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,40 +1,40 @@
 @echo off
 setlocal
 
-:: Pfad des aktuellen Skripts abrufen
+:: path
 set "CURRENT_DIR=%~dp0"
 
-:: Check if Python is installed
-echo Checking for Python installation...
+:: check if python is installed
+echo check python
 for %%i in (python.exe) do set "PYTHON_PATH=%%~$PATH:i"
 
 if "%PYTHON_PATH%"=="" (
-    echo Python is not installed. Downloading and installing Python...
-    curl -o python-installer.exe https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.exe
+    echo Python is not installed. Getting installed...
+    curl -o python-installer.exe https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe
     start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
     del python-installer.exe
-    echo Python installed successfully.
+    echo Python successfully installed.
 ) else (
     echo Python is already installed.
 )
 
-:: Upgrade pip and install required Python packages
-echo Installing Python packages...
+:: pip
+echo installation of pip and python packages
 python -m pip install --upgrade pip
-python -m pip install yt-dlp pygame mutagen pypresence
+python -m pip install yt-dlp pygame mutagen pypresence websockets
 
-echo Python packages installed successfully.
+echo Python-Packets successfully installed
 
-:: Check if ffmpeg is in the same directory as the script
-echo Checking for FFmpeg installation in the script directory...
-if exist "ffmpeg-7.1-essentials_build/bin/ffmpeg.exe" (
-    echo FFmpeg is already in the script directory.
+:: check ffmpeg
+echo Checking for ffmeg
+if exist "%CURRENT_DIR%ffmpeg-7.1-essentials_build\bin\ffmpeg.exe" (
+    echo ffmeg is already installed
 ) else (
-    echo FFmpeg is not found in the script directory. Downloading and installing FFmpeg...
+    echo FFmpeg not found. installing...
     curl -L -o ffmpeg.zip https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
     tar -xf ffmpeg.zip
     del ffmpeg.zip
-    echo FFmpeg installed successfully in the script directory.
+    echo FFmpeg successfully installed
 )
 
 endlocal
