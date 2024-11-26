c97a90fdeab6bda5143de46ed43c0f811a8bc8c8
Tamino <roidingertamino@gmail.com>
2024-11-26 12:02:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-by = "tamino1230"
+by = "Tamino1230"
+
+bgcolor = "#F791C1" # background-color | #F791C1 is a hex color
+
+default_folder = "downloaded_music" # where the downloaded files are getting added to. (which folder)
 
 # if any problems happen text me on discord: @tamino1230
 
