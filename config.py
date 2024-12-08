6b803c00d02f49dd50111866178159606ff784f8
Tamino <roidingertamino@gmail.com>
2024-12-08 14:49:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 by = "Tamino1230"
 
-bgcolor = "#F791C1" # background-color | #F791C1 is a hex color
+bgcolor = "lightblue" # background-color | #F791C1 is a hex color #F791C1
 
 default_folder = "downloaded_music" # where the downloaded files are getting added to. (which folder)
+
+record_actions = True # If you want to record all your actions / False = no / True = yes
 
 # if any problems happen text me on discord: @tamino1230
 
