9c693ebc846dcd4acd0bda2a66aa217f6244258d
Tamino <roidingertamino@gmail.com>
2024-12-09 13:19:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,32 @@
 by = "Tamino1230"
 
-bgcolor = "lightblue" # background-color | #F791C1 is a hex color #F791C1
+bgcolor = "#F791C1" # background-color | #F791C1 is a hex color #F791C1
 
 default_folder = "downloaded_music" # where the downloaded files are getting added to. (which folder)
 
 record_actions = True # If you want to record all your actions / False = no / True = yes
 
+show_repeat_shuffle = True # If False it will not show (Repeat & Shuffle) on Discord
+
+error_message = False # Debugging tool
+
+default_discord_rich_presence = True # If its on False it will not show it on discord but it will still show ON in the Programm
+
+hardcoded_presence = " | made by tamino1230 on GitHub <3" # Programm will not work if changed
+
 # if any problems happen text me on discord: @tamino1230
+
+# custom rich presence settings:
+# playing:
+playing_presence = f"Listening to "
+playing_custom_text_behind = ""
+
+# Paused:
+paused_presence = f"Paused "
+paused_custom_text_behind = ""
+
+# Idling:
+idle_presence = f"Idling "
+idle_custom_text_behind = ""
 
 # have fun with this "music app"
