24b6b5d94476b84698abaa07772e517594873800
Tamino <roidingertamino@gmail.com>
2024-12-12 17:25:19 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 by = "Tamino1230"
 
-bgcolor = "lightblue" # background-color | #F791C1 is a hex color #F791C1
+bgcolor = "#F791C1" # background-color | #F791C1 is a hex color #F791C1
 
 default_folder = "downloaded_music" # where the downloaded files are getting added to. (which folder)
 
@@ -8,19 +8,23 @@ record_actions = True # If you want to record all your actions / False = no / Tr
 
 show_repeat_shuffle = True # If False it will not show (Repeat & Shuffle) on Discord
 
-error_message = False # Debugging tool.
+error_message = True # Debugging tool.
 # Put on True if you programm is not starting and when problems apear
 
 default_discord_rich_presence = True # default True
 
+max_volume = 200 # need to be under 500 and over 0
+
 # do not change
-hardcoded_config = "config.py"
-hardcoded_resizeable = False
-hardcoded_geometry = "800x600"
-hardcoded_root_title = "MusiEz - @tamino1230"
+hardcoded_config = "config.py" # Programm will not work if changed
+hardcoded_resizeable = False # Programm will not work if changed
+hardcoded_geometry = "800x600" # Programm will not work if changed
+hardcoded_root_title = "MusiEz - @tamino1230" # Programm will not work if changed
 hardcoded_presence = " | made by tamino1230 on GitHub <3" # Programm will not work if changed
 hardcoded_client_id = '1309941984407977996' # Programm will not work if changed
 hardcoded_icon_path = "icon/babToma.ico" # Programm will not work if changed
+
+
 
 # if any problems happen text me on discord: @tamino1230
 
@@ -39,6 +43,6 @@ idle_custom_text_behind = ""
 
 # Only custom rich presence
 only_custom_rpc = False # False on default
-custom_rpc_text = "Exampletext"
+custom_rpc_text = "Exampletext!!!!"
 
 # have fun with this "music app"
